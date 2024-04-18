bea674f0df5ee1ab8a756635d4e9fc8a8e152b4d
Sergey <spumen35@gmail.com>
2024-04-18 09:57:25 +0300

    .\Cat.java

---
+++ Cat.java
@@ -0,0 +1,19 @@
+package Task6;
+public class Cat {
+    private String name;
+
+    public Cat() {
+    }
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public void voice(){
+        System.out.println("мяу!");
+    }
+
+    public String getName() {
+        return name;
+    }
+}
